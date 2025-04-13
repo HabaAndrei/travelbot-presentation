e8d27c42cf8fa1d7743c2b414b72ac309f78bb4a
Haba Andrei <habaconstantin45@gmail.com>
2025-04-13 16:14:19 +0300

    pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,51 +1,21 @@
-import Image from "next/image";
 import GeneralDescription from "./Components/GeneralDescription.js";
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center">
-        <Image
-          src="/travelbot.svg"
-          alt="Travelbot logo"
-          width={180}
-          height={38}
-          priority
-        />
-
+    <div className=" items-center justify-items-center p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col row-start-2 items-center">
         <GeneralDescription/>
 
+        <Link
+          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+          href="/functionalities"
+          rel="noopener noreferrer"
+        >
+          Functionalities
+        </Link>
+
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/gmail.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          travelbot.suport@gmail.com
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://www.linkedin.com/in/andrei-haba-1007942aa/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            src="/linkedin.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Developer account →
-        </a>
-      </footer>
     </div>
   );
 }
