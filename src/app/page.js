0dff17b4494be1c49c4aaae216e5535df1876825
Haba Andrei <habaconstantin45@gmail.com>
2025-04-13 15:11:52 +0300

    import GeneralDescription

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,19 @@
 import Image from "next/image";
+import GeneralDescription from "./Components/GeneralDescription.js";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          src="/travelbot.svg"
-          alt="Travelbot logo"
-          width={180}
-          height={38}
-          priority
-        />
+<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+  <main className="flex flex-col gap-[32px] row-start-2 items-center">
+    <Image
+      src="/travelbot.svg"
+      alt="Travelbot logo"
+      width={180}
+      height={38}
+      priority
+    />
 
+        <GeneralDescription/>
 
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
