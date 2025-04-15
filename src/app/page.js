8e6d707271365b3c0a1b44c56c020b57c87c5764
Haba Andrei <habaconstantin45@gmail.com>
2025-04-15 13:40:24 +0300

    .

--- src/app/page.js
+++ src/app/page.js
@@ -8,10 +8,10 @@ export default function Home() {
         <GeneralDescription/>
 
         <Link
-          className="rounded-full border border-solid border-black/[.08] border-white/[.145] transition-colors flex items-center justify-center bg-[#f2f2f2] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+          className="rounded-full border border-solid border-black/[.08] flex items-center justify-center bg-[#f2f2f2] font-medium text-black text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
           href="/functionalities"
           rel="noopener noreferrer"
-          >
+        >
           Functionalities
         </Link>
 
