f0a00a9679a38620acf2ae4ad6cd52579f455a14
Haba Andrei <habaconstantin45@gmail.com>
2025-05-04 18:12:00 +0300

    edit footer

--- src/app/Components/Footer.js
+++ src/app/Components/Footer.js
@@ -5,6 +5,34 @@ export default function Footer() {
     <footer className="py-6 border-t flex gap-6 flex-wrap items-center justify-center text-sm text-white">
       <a
         className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://apps.apple.com/us/app/travel-bot/id6744552358"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          src="/app-store.svg"
+          alt="App store icon"
+          width={16}
+          height={16}
+        />
+        Download from App Store →
+      </a>
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://play.google.com/store/apps/details?id=com.haba03.expoapp"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          src="/google-play.svg"
+          alt="Google play icon"
+          width={16}
+          height={16}
+        />
+        Download from Google play →
+      </a>
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
         href="mailto:travelbot.suport@gmail.com"
         target="_blank"
         rel="noopener noreferrer"
@@ -16,7 +44,7 @@ export default function Footer() {
           width={16}
           height={16}
         />
-        travelbot.suport@gmail.com
+        Suport: travelbot.suport@gmail.com
       </a>
       <a
         className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -31,34 +59,6 @@ export default function Footer() {
           height={16}
         />
         Developer account →
-      </a>
-      <a
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href="https://apps.apple.com/us/app/travel-bot/id6744552358"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          src="/app-store.svg"
-          alt="App store icon"
-          width={16}
-          height={16}
-        />
-        App in App Store →
-      </a>
-      <a
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href="https://play.google.com/store/apps/details?id=com.haba03.expoapp"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          src="/google-play.svg"
-          alt="Google play icon"
-          width={16}
-          height={16}
-        />
-        App in Google play →
       </a>
     </footer>
   );
