701c5fc96a0a100b5e57f9edb5266370b0b80a75
Haba Andrei <habaconstantin45@gmail.com>
2025-05-08 21:26:25 +0300

    add google analistics tags

--- src/app/Components/Header.js
+++ src/app/Components/Header.js
@@ -1,17 +1,34 @@
 import Image from "next/image";
+import Script from "next/script";
 
 export default function Header() {
   return (
-  <hearder className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    <div className="flex flex-col gap-[32px] row-start-2 items-center">
-      <Image
-        src="/travelbot.svg"
-        alt="Travelbot logo"
-        width={180}
-        height={38}
-        priority
+    <>
+      {/* Google scripts */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-8JJ76QCHTE"
+        strategy="afterInteractive"
       />
-    </div>
-  </hearder>
-  )
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-8JJ76QCHTE');
+        `}
+      </Script>
+
+      <header className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <div className="flex flex-col gap-[32px] row-start-2 items-center">
+          <Image
+            src="/travelbot.svg"
+            alt="Travelbot logo"
+            width={180}
+            height={38}
+            priority
+          />
+        </div>
+      </header>
+    </>
+  );
 }
